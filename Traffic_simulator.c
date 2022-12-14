61c653bd7f1757357e29e402e8bd28e564d99ee1
lior <lior922@gmail.com>
2022-12-14 15:06:36 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/Traffic-simulator.iml

--- Traffic_simulator.c
+++ Traffic_simulator.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
-
+#define N 5
+#define FIN_PROB 0.1
+#define MIN_INTER_ARRIVAL_IN_NS 8000000
+#define MAX_INTER_ARRIVAL_IN_NS 9000000
+#define INTER_MOVES_IN_NS 100000
+#define SIM_TIME 2
 
 int main(){
 
